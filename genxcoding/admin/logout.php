5322dd6ce27edce0561ec0b48d6f8f6b4af830aa
Aakanksha Mandal <aku.mandal@gmail.com>
2026-07-20 03:16:19 -0400

    Delete all files for a reupload

--- genxcoding/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// admin/logout.php - clears the admin session and sends them back to login.
-require_once '../config.php';
-unset($_SESSION['admin_id']);
-unset($_SESSION['admin_username']);
-header("Location: login.php");
-exit;
-?>
--- genxcoding/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config.php';
-// wipe the session and go home
-session_destroy();
-header("Location: index.php");
-exit;
-?>
